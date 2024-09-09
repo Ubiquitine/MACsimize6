1d72a380101e58e60bf7dea9f3c6b7721dbeee67
Evgeniy Gurevich <evgeniy.gurevich@gmail.com>
2024-09-09 17:02:51 +0800

    Exclude 'kwin' window class

--- contents/code/main.js
+++ contents/code/main.js
@@ -10,7 +10,7 @@ const savedDesktops = {};
 const savedModes = {};
 const savedHandlers = {};
 
-const systemSkippedWindows = ['kwin_wayland', 'ksmserver-logout-greeter', 'ksmserver',
+const systemSkippedWindows = ['kwin', 'kwin_wayland', 'ksmserver-logout-greeter', 'ksmserver',
     'kscreenlocker_greet', 'ksplash', 'ksplashqml', 'plasmashell', 'org.kde.plasmashell', 'krunner'];
 var configSkippedWindows = readConfig("SkipWindows", "lattedock, latte-dock, org.kde.spectacle").toString().toLowerCase().split(/,\s*/);
 var alwaysSkippedWindows = systemSkippedWindows.concat(configSkippedWindows)
